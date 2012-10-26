db480ef61aa5e0dd5ea0d7349072d5da77cc1e78
Beat Oberholzer <oberholzer@filmwords.com>
2012-10-26 07:20:28 +0200

    Added page3

--- public/index.php
+++ public/index.php
@@ -3,3 +3,5 @@
 <h2>This line is added in the development branch</h2>
 
 <p>Current time: <?php echo date('H:i:s'); ?></p>
+
+<a href="page3.html">link to Page3 (added in dev branch directly, not in feature branch)</a>
