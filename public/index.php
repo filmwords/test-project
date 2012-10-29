86bffdcdf2c81d697ef577e4607650eb28cc6f72
Beat Oberholzer <oberholzer@filmwords.com>
2012-10-29 16:32:06 +0100

    Added content from monitoring page

--- public/index.php
+++ public/index.php
@@ -4,5 +4,9 @@
 
 <p>Current time: <?php echo date('H:i:s'); ?></p>
 
+<div>
+    <?php echo file_get_contents('http://filmwords.com/monitoring/?embed=1'); ?>
+</div>
+
 <p><a href="page2.php">Go to page 2</a></p>
 <p><a href="page3.html">link to Page3 (added in dev branch directly, not in feature branch)</a></p>
